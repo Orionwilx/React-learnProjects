9b9fc599b2381df5a8bd4db12b2c2e08d020debd
Orion <wilman3d@hotmail.com>
2025-04-05 08:33:37 -0500

    feat: initialize mouse follower app with React and Vite
    - Added package.json with dependencies and scripts for development.
    - Created public assets including Vite logo SVG.
    - Implemented main application structure in App.jsx with mouse follow functionality.
    - Styled application with App.css and index.css for layout and design.
    - Configured Vite for building and serving the application.
    - Added React and Vite related SVG assets.

---
+++ mouse-follower/src/App.jsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from "react";
+
+const FollowMouse = () => {
+  const [enable, setEnable] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    console.log("useEffect" + enable);
+
+    const handleMove = (e) => {
+      const { clientX, clientY } = e;
+      setPosition({ x: clientX, y: clientY });
+    };
+    if (enable) {
+      window.addEventListener("mousemove", handleMove);
+    }
+
+    return () => {
+      console.log("removeEventListener" + enable);
+      window.removeEventListener("mousemove", handleMove);
+    };
+  }, [enable]);
+
+  return (
+    <>
+      <div
+        style={{
+          position: "absolute",
+          backgroundColor: "rgba(0, 0, 0, 0.5)",
+          border: "1px solid #fff",
+          borderRadius: "50%",
+          opacity: 0.8,
+          pointerEvents: "none",
+          left: -25,
+          top: -25,
+          width: 50,
+          height: 50,
+          transform: `translate(${position.x}px, ${position.y}px)`,
+        }}
+      ></div>
+      <button onClick={() => setEnable(!enable)}>
+        {enable ? "Ativar" : "Desactivar"} Seguir puntero
+      </button>
+    </>
+  );
+};
+
+function App() {
+  return (
+    <main>
+      <FollowMouse />
+    </main>
+  );
+}
+
+export default App;
