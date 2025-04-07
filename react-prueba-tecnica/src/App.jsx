4598b1404963e8066203ab6073fa63f410efae9f
Orion <wilman3d@hotmail.com>
2025-04-07 11:28:43 -0500

    feat: add CSS styles for main layout and image display; refactor App component for improved functionality

--- react-prueba-tecnica/src/App.jsx
+++ react-prueba-tecnica/src/App.jsx
@@ -1,28 +1,32 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import "./App.css";
 
 const CAT_ENDPOINT_UMAGE_URL = "https://cataas.com/cat/says/";
 const CAT_ENDPOINT_RANDOM_FACT = "https://catfact.ninja/fact";
 
 export function App() {
-  const [fact, setfact] = useState("lorem ipsu cat fact");
+  const [fact, setfact] = useState("");
   const [imageUrl, setImageUrl] = useState();
 
+  // Recuperar frase random de la API de catfact
   useEffect(() => {
     fetch(CAT_ENDPOINT_RANDOM_FACT)
       .then((res) => res.json())
       .then((data) => {
         const { fact } = data;
         setfact(fact);
-
-        const firstWord = fact.split(" ").slice(0, 3).join(" ");
-        console.log(firstWord);
-
-        fetch(`${CAT_ENDPOINT_UMAGE_URL}${firstWord}`).then((res) => {
-          setImageUrl(`${CAT_ENDPOINT_UMAGE_URL}${firstWord}`);
-        });
       });
   }, []);
+  // Recuperar imagen random de la API de cataas
+  useEffect(() => {
+    if (!fact) return;
+    const firstWord = fact.split(" ").slice(0, 3).join(" ");
+
+    fetch(`${CAT_ENDPOINT_UMAGE_URL}${firstWord}`).then((res) => {
+      setImageUrl(`${CAT_ENDPOINT_UMAGE_URL}${firstWord}`);
+    });
+  }, [fact]);
 
   return (
     <main>
